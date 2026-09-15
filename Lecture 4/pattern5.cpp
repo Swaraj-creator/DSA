9c88819df6c70a7f100ecc16aed258a1e864f34c
Swaraj-creator <swarajkumar7011@gmail.com>
2026-09-15 15:12:20 +0530

    Made some changes in program5

--- Lecture 4/pattern5.cpp
+++ Lecture 4/pattern5.cpp
@@ -6,8 +6,8 @@ int main() {
     cout << "Enter a Range: ";
     cin >> n;
 
-    for(int i = n; i > 0; i--) {
-        for(int j = 0; j <= n - i; j++) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j <= i; j++) {
             cout << "*" << " ";
         }
         cout << "\n";
